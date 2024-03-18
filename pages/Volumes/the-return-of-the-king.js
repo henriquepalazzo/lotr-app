dbd5299210ca1f9e85e5b15da38f79a0f14f74cb
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-03-18 15:27:21 +0100

    Add logic for pagination and Nav element to the detail pages.

--- pages/Volumes/the-return-of-the-king.js
+++ pages/Volumes/the-return-of-the-king.js
@@ -1,24 +1,30 @@
 import Link from "next/link";
 import Image from "next/image";
 import { volumes } from "../../lib/data";
+import Nav from "@/components/Nav";
 
 export default function TheReturnOfTheKing() {
-  const volume = volumes.find(
+  const volumeIndex = volumes.findIndex(
     (volume) => volume.slug === "the-return-of-the-king"
   );
+  const volume = volumes[volumeIndex];
+
   return (
-    <article>
-      <h1>{volume.title}</h1>
-      <p>{volume.description}</p>
-      <ul>
-        {volume.books.map((book, index) => (
-          <li key={index}>{book.title}</li>
-        ))}
-      </ul>
-      <Image src={volume.cover} width={140} height={230} alt={volume.title} />
-      <div>
-        <Link href="/Volumes">← All Volumes</Link>
-      </div>
-    </article>
+    <>
+      <article>
+        <h1>{volume.title}</h1>
+        <p>{volume.description}</p>
+        <ul>
+          {volume.books.map((book, index) => (
+            <li key={index}>{book.title}</li>
+          ))}
+        </ul>
+        <Image src={volume.cover} width={140} height={230} alt={volume.title} />
+        <div>
+          <Link href="/Volumes">← All Volumes</Link>
+        </div>
+      </article>
+      <Nav currIndex={volumeIndex} pages={volumes} />
+    </>
   );
 }
--- pages/Volumes/the-two-towers.js
+++ pages/Volumes/the-two-towers.js
@@ -1,23 +1,30 @@
 import Link from "next/link";
 import Image from "next/image";
 import { volumes } from "../../lib/data";
+import Nav from "@/components/Nav";
 
 export default function TheTwoTowers() {
-  const volume = volumes.find((volume) => volume.slug === "the-two-towers");
-  console.log("VOLUME: ", volume);
+  const volumeIndex = volumes.findIndex(
+    (volume) => volume.slug === "the-two-towers"
+  );
+  const volume = volumes[volumeIndex];
+
   return (
-    <article>
-      <h1>{volume.title}</h1>
-      <p>{volume.description}</p>
-      <ul>
-        {volume.books.map((book, index) => (
-          <li key={index}>{book.title}</li>
-        ))}
-      </ul>
-      <Image src={volume.cover} width={140} height={230} alt={volume.title} />
-      <div>
-        <Link href="/Volumes">← All Volumes</Link>
-      </div>
-    </article>
+    <>
+      <article>
+        <h1>{volume.title}</h1>
+        <p>{volume.description}</p>
+        <ul>
+          {volume.books.map((book, index) => (
+            <li key={index}>{book.title}</li>
+          ))}
+        </ul>
+        <Image src={volume.cover} width={140} height={230} alt={volume.title} />
+        <div>
+          <Link href="/Volumes">← All Volumes</Link>
+        </div>
+      </article>
+      <Nav currIndex={volumeIndex} pages={volumes} />
+    </>
   );
 }
