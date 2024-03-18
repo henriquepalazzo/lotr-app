239ed00b1ad7354d274bcde8fe311f778afacebe
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-03-18 17:07:44 +0100

    Add dynamic detail page.

---
+++ pages/Volumes/[slug].js
@@ -0,0 +1,24 @@
+import { volumes } from "../../lib/data";
+import Nav from "@/components/Nav";
+import VolumeDetail from "@/components/VolumeDetail";
+import Head from "next/head";
+import { useRouter } from "next/router";
+
+export default function TheFellowshipOfTheRing() {
+  const router = useRouter();
+  const { slug } = router.query;
+
+  const volumeIndex = volumes.findIndex((volume) => volume.slug === slug);
+  const volume = volumes[volumeIndex];
+
+  return (
+    <>
+      <Head>
+        <title>Lord of the Rings - {volume.title}</title>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </Head>
+      <VolumeDetail volume={volume} />
+      <Nav currIndex={volumeIndex} pages={volumes} />
+    </>
+  );
+}
