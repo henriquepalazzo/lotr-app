f2b429b37f134492a5c1eda57b9e0f97e91803cf
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-03-19 18:54:09 +0100

    Add component AllVolumes to the volumes template.

--- pages/Volumes/index.js
+++ pages/Volumes/index.js
@@ -3,6 +3,7 @@ import Head from "next/head";
 import { introduction } from "../../lib/data";
 import { volumes } from "../../lib/data";
 import { useRouter } from "next/router";
+import AllVolumes from "@/components/Volumes/Volumes";
 
 export default function Volumes() {
   const router = useRouter();
@@ -19,16 +20,7 @@ export default function Volumes() {
       <section>
         <h1>The Lord of The Rings</h1>
         <p>{introduction}</p>
-        <h2>All Volumes</h2>
-        <ul>
-          {volumes.map((volume, index) => {
-            return (
-              <li key={index}>
-                <Link href={`Volumes/${volume.slug}`}>{volume.title}</Link>
-              </li>
-            );
-          })}
-        </ul>
+        <AllVolumes volumes={volumes} />
         <button onClick={handleRandonGeneratorClick}>
           Go to randon volume
         </button>
